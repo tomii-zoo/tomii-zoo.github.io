db5adb2e52ad5e562fee6de979be89939b0a6826
tomii-zoo <tomii@zoo.co.jp>
2022-04-17 21:26:32 +0900

    add textarea I/O

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("test");
+const LSKey = "edit"
+const TextAreaID = "edit";
+const InitializeValue = "input your text...";
+
+/**
+ * Load Local Storage
+ */
+function loadLS() {
+  const textarea = document.getElementById(TextAreaID);
+  const data = localStorage.getItem(LSKey);
+  if (data != null) {
+    textarea.value = data;
+  } else {
+    textarea.value = InitializeValue;
+  }
+  console.log("load localstorage => " + textarea.value);
+}
+
+/**
+ * Save Local Storage
+ */
+function saveLS() {
+  const textarea = document.getElementById(TextAreaID);
+  localStorage.setItem(LSKey, textarea.value);
+  console.log("save localstorage => " + textarea.value);
+}
