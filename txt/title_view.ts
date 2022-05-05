0ff6e1badb4174cf83303e14c185821db39d6ba6
tomii-zoo <tomii@zoo.co.jp>
2022-05-06 01:13:44 +0900

    Create title_view.ts

---
+++ txt/title_view.ts
@@ -0,0 +1,40 @@
+
+function log(msg: any) {
+	console.log(msg);
+}
+
+class TitleView {
+  private m_SelectedMenu = -1;
+  private m_IsEnd = false;
+
+  private c_MenuStart = 0;
+  private c_MenuConfig = 1;
+  private c_MenuExit = 2;
+  private c_Msg = "Menus: start(0), config(1), exit(2)";
+  /**
+   * select menu
+   */
+  Update() {
+	const input = window.prompt(this.c_Msg);
+	this.m_SelectedMenu = Number(input);
+
+	if (this.m_SelectedMenu == this.c_MenuStart) {
+		log("start");
+	}
+	else if (this.m_SelectedMenu == this.c_MenuConfig) {
+		log("config");
+	}
+	else if (this.m_SelectedMenu == this.c_MenuExit) {
+		log("exit");
+	}
+  }
+
+  public IsEnd() : boolean {
+	return this.m_SelectedMenu >= 0
+  }
+}
+
+const view = new TitleView();
+while (!view.IsEnd()) {
+	view.Update();
+}
